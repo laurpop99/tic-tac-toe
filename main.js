a090e2704f27a834adcf65391290fee6cbf8417b
Popescu Laurentiu <laurpop99@yahoo.com>
2021-10-17 18:03:02 +0300

    Added AI Functionality

--- main.js
+++ main.js
@@ -11,6 +11,71 @@ const gameBoard = (function(){
 
 //displayController Module
 const displayController = (function(){
+    const start = document.querySelector(".start");
+    const restart = document.querySelector(".restart");
+    const checkbox = document.querySelector(".checkbox");
+    
+    checkbox.addEventListener("click", function(){
+        checkGame();
+        removeBoard();
+
+        
+
+    })
+
+    const computerStart = () => {
+        playerOne = playerFactory("Player 1", "X");
+        playerTwo = playerFactory("Player 2", "0");
+        playerOne.name = prompt("Player X Name: ");
+        if(playerOne.name === null) playerOne.name = "Player 1";
+        if(typeof(playerOne) !== "undefined"){
+            restart.addEventListener("click", function(){
+                removeBoard();
+                displayController.displayBoardComputer();
+            })
+        }
+        removeBoard();
+        displayController.displayBoardComputer();
+    }
+
+    const playerStart = () => {
+        
+            playerOne = playerFactory("Player 1", "X");
+            playerTwo = playerFactory("Player 2", "0");
+            playerOne.name = prompt("Player X Name: ");
+            playerTwo.name = prompt("Player 0 Name: ");
+            if(playerOne.name === null) playerOne.name = "Player 1";
+            if(playerTwo.name === null) playerTwo.name = "Player 2";
+            if(typeof(playerOne) !== "undefined"){
+                restart.addEventListener("click", function(){
+                    removeBoard();
+                    displayController.displayBoard();
+                })
+            }
+            removeBoard();
+            displayController.displayBoard();
+  
+    }
+    
+     const checkGame = () => {
+        if(checkbox.checked){
+            // console.log("CHECKED"); 
+            start.addEventListener("click", computerStart);
+            start.removeEventListener("click", playerStart);
+
+        }
+            
+         else{
+            // console.log("UNCHECKED");
+            start.addEventListener("click", playerStart);
+            start.removeEventListener("click", computerStart);
+            
+         }
+     }  
+
+     checkbox.checked = false;
+    checkGame();
+    
     let tilesArr = [];
     let isMyTurn = true;
     const removeBoard = () => {
@@ -21,7 +86,54 @@ const displayController = (function(){
         isMyTurn = true;
         for(let i = 0; i < 9; i++){
         gameBoard.content[i] = "";
+        gameBoard.header.textContent = "";
         }
+        
+        
+    }
+    const displayBoardComputer = () => {
+        for(let i = 0; i < gameBoard.length; i++){
+            const tiles = document.createElement("div");
+            tiles.classList.add("tiles");
+            tiles.textContent = gameBoard.content[i];
+            gameBoard.board.appendChild(tiles);
+            tilesArr.push(tiles);
+            
+            }
+
+            tilesArr.forEach(function(tile){
+                tile.addEventListener("click", function(){
+
+                    const tileIndex = tilesArr.indexOf(this);
+                    if (gameBoard.content[tileIndex] === "") {
+                        gameBoard.content[tileIndex] = playerOne.sign;
+                        tilesArr[tileIndex].textContent = playerOne.sign;
+                        computerTurn();
+                        checkWinner(playerTwo);
+                        checkWinner(playerOne);
+                        
+                    }
+                    
+                    
+                    
+                    
+                    
+                })
+            })
+    }
+
+    const computerTurn = () => {
+        let randomSpot = Math.floor(Math.random() * gameBoard.content.length);
+        while (gameBoard.content[randomSpot] !== "" && gameBoard.content.includes("")){
+            randomSpot = Math.floor(Math.random() * gameBoard.content.length);
+        }
+        const answer = randomSpot;
+        gameBoard.content[answer] = playerTwo.sign;
+        tilesArr[answer].textContent = playerTwo.sign;
+        // if (gameBoard.content[answer] === ""){
+        // gameBoard.content[answer] = playerTwo.sign;
+        // tilesArr[answer].textContent = playerTwo.sign;
+        // }
         
         
     }
@@ -35,8 +147,14 @@ const displayController = (function(){
             gameBoard.board.appendChild(tiles);
             tilesArr.push(tiles);
             
-            
             }
+
+            if(isMyTurn){
+                gameBoard.header.textContent = `${playerOne.name}'s turn`;
+                }
+                else {
+                    gameBoard.header.textContent = `${playerTwo.name}'s turn`;
+                }
             
             tilesArr.forEach(function(tile){
                 
@@ -48,6 +166,13 @@ const displayController = (function(){
                         tilesArr[tileIndex].textContent = playerOne.sign;
                         isMyTurn = !isMyTurn; 
                         checkWinner(playerOne);
+                        if(isMyTurn){
+                            gameBoard.header.textContent = `${playerOne.name}'s turn`;
+                            }
+                            else {
+                                gameBoard.header.textContent = `${playerTwo.name}'s turn`;
+                            }
+                        
                     }
                     
                     else if(!isMyTurn && gameBoard.content[tileIndex] === "") {
@@ -55,6 +180,13 @@ const displayController = (function(){
                         tilesArr[tileIndex].textContent = playerTwo.sign;
                         isMyTurn = !isMyTurn;
                         checkWinner(playerTwo);
+                        if(isMyTurn){
+                            gameBoard.header.textContent = `${playerOne.name}'s turn`;
+                            }
+                            else {
+                                gameBoard.header.textContent =`${playerTwo.name}'s turn`;
+                            }
+                        
                     }
                 })
             })
@@ -118,7 +250,12 @@ const displayController = (function(){
             alert(`${player.name} won the game!`);
             player.hasWon = false;
             removeBoard();
-            displayBoard();
+            if(checkbox.checked){
+            displayBoardComputer();
+            }
+            else{
+                displayBoard();
+            }
             
         }
         
@@ -131,7 +268,12 @@ const displayController = (function(){
             
             alert("It's a tie!");
             removeBoard();
-            displayBoard();
+            if(checkbox.checked){
+            displayBoardComputer();
+            }
+            else{
+                displayBoard();
+            }
         }
     }
         
@@ -140,7 +282,7 @@ const displayController = (function(){
 
 
     
-    return {tilesArr, displayBoard, isMyTurn};
+    return {tilesArr, displayBoard, isMyTurn, checkbox, start, displayBoardComputer, computerTurn};
 })();
 
 
@@ -150,9 +292,10 @@ const displayController = (function(){
 const playerFactory = (name, sign) => {
     const sayName = () => console.log(`Hey, my name is ${name} and I chose ${sign}`);
     const hasWon = false;
+    
     return {name, sayName, sign, hasWon};
 }
 
-displayController.displayBoard();
-const playerOne = playerFactory("Larry", "X");
-const playerTwo = playerFactory("Geani", "0");
+
+
+
